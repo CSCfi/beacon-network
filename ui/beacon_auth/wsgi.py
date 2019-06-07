7fcea0f9d094787168033d3219dfc5376050f32e
Teemu Kataja <teemu.kataja@csc.fi>
2019-06-07 15:30:02 +0300

    Add ga4gh scope

--- ui/beacon_auth/wsgi.py
+++ ui/beacon_auth/wsgi.py
@@ -53,7 +53,7 @@ def make_authorization_url():
               "state": state,
               "redirect_uri": CALLBACK_URL,
               "duration": "temporary",
-              "scope": 'openid bona_fide_status permissions_rems'}
+              "scope": 'openid bona_fide_status permissions_rems ga4gh'}
     url = "https://login.elixir-czech.org/oidc/authorize?" + urllib.parse.urlencode(params)
     return url
 
