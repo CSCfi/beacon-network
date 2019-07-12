55eb64a90822419f48b82d96f4541d748d6a9fe1
Teemu Kataja <teemu.kataja@csc.fi>
2019-07-12 08:57:33 +0300

    update installation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='beacon_network',
-    version='0.1.dev',
+    version='0.2.dev',
     description='Beacon Network services',
     long_description_content_type='text/markdown',
     project_urls={
@@ -16,17 +16,20 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3.6',
     ],
-    packages=['.', 'config', 'endpoints', 'schemas', 'utils'],
+    packages=['aggregator', 'aggregator/config', 'aggregator/endpoints',
+              'aggregator/utils', 'registry', 'registry/config',
+              'registry/db', 'registry/endpoints', 'registry/schemas',
+              'registry/utils'],
     package_data={'': ['*.json', '*.ini']},
     install_requires=[
         'aiohttp', 'asyncpg', 'aiohttp_cors', 'uvloop',
         'asyncio', 'aiocache', 'aiomcache', 'ujson',
-        'jsonschema==3.0.0a3'
+        'jsonschema==3.0.1'
     ],
     entry_points={
         'console_scripts': [
-            'beacon_registry=registry:main',
-            'beacon_aggregator=aggregator:main'
+            'beacon_registry=registry.registry:main',
+            'beacon_aggregator=aggregator.aggregator:main'
         ],
     },
 )
