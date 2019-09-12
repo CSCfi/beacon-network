2d5f891f6cf4b534894997bbf72485716cbddf15
Teemu Kataja <teemu.kataja@csc.fi>
2019-09-12 10:57:31 +0300

    add more database unit tests

--- tests/test_db_ops.py
+++ tests/test_db_ops.py
@@ -5,7 +5,8 @@ from aiohttp import web
 from registry.utils.db_pool import init_db_pool
 from registry.utils.db_ops import db_check_service_id, db_store_service_key, db_update_service_key
 from registry.utils.db_ops import db_delete_service_key, db_store_email, db_register_service
-from registry.utils.db_ops import db_get_service_details, db_delete_services
+from registry.utils.db_ops import db_get_service_details, db_delete_services, db_update_service
+from registry.utils.db_ops import db_update_sequence, db_verify_service_key, db_verify_api_key, db_verify_admin_key
 
 from .db_test_classes import Connection, BadConnection
 
@@ -170,6 +171,103 @@ class TestDatabaseOperations(asynctest.TestCase):
         connection = BadConnection()
         with self.assertRaises(web.HTTPInternalServerError):
             await db_delete_services(connection, 'fi.beacon')
+
+    async def test_db_update_service_success(self):
+        """Test the updating of a service: successful update."""
+        connection = Connection()
+        service_info = {
+            'id': 'id',
+            'name': 'name',
+            'type': 'type',
+            'description': 'desc',
+            'url': 'url',
+            'contact_url': 'contact',
+            'api_version': 'api',
+            'service_version': 'version',
+            'environment': 'env',
+            'organization': 'org',
+            'organization_url': 'url',
+            'organization_logo': 'logo'
+        }
+        await db_update_service(connection, 'id', service_info)
+        # No exceptions raised
+
+    @asynctest.mock.patch('registry.utils.db_ops.db_check_service_id')
+    async def test_db_update_service_conflict(self, mock_id):
+        """Test the updating of a service: service id taken."""
+        mock_id.return_value = 'new.id'
+        connection = Connection()
+        with self.assertRaises(web.HTTPConflict):
+            await db_update_service(connection, 'old.id', {'id': 'new.id'})
+
+    async def test_db_update_service_fail(self):
+        """Test the updating of a service: failed to update."""
+        connection = BadConnection()
+        with self.assertRaises(web.HTTPInternalServerError):
+            await db_update_service(connection, 'fi.beacon', {'id': 'fi.beacon'})
+
+    @asynctest.mock.patch('registry.utils.db_ops.db_update_service_key')
+    @asynctest.mock.patch('registry.utils.db_ops.db_update_service')
+    async def test_db_update_sequence(self, mock_service, mock_key):
+        """Test update sequence."""
+        connection = Connection()
+        mock_service.return_value = True
+        mock_key.return_value = True
+        await db_update_sequence(connection, 'fi.beacon', {'id': 'fi.beacon'})
+
+    async def test_db_verify_service_key_success(self):
+        """Test the verification of service key: successful verification."""
+        connection = Connection(return_value=[{'service_id': 'fi.beacon'}])
+        await db_verify_service_key(connection, 'fi.beacon', 'abc123')
+        # No exceptions raised
+
+    async def test_db_verify_service_key_fail(self):
+        """Test the verification of service key: failed to verify."""
+        connection = BadConnection()
+        with self.assertRaises(web.HTTPInternalServerError):
+            await db_verify_service_key(connection, 'fi.beacon', 'abc123')
+
+    async def test_db_verify_service_key_unauthorized(self):
+        """Test the verification of service key: unauthorized key."""
+        connection = Connection(return_value=[])
+        with self.assertRaises(web.HTTPUnauthorized):
+            await db_verify_service_key(connection, 'fi.beacon', 'abc123')
+
+    async def test_db_verify_api_key_success(self):
+        """Test the verification of api key: successful verification."""
+        connection = Connection(return_value=[{'comment': 'dev'}])
+        await db_verify_api_key(connection, 'abc123')
+        # No exceptions raised
+
+    async def test_db_verify_api_key_fail(self):
+        """Test the verification of api key: failed to verify."""
+        connection = BadConnection()
+        with self.assertRaises(web.HTTPInternalServerError):
+            await db_verify_api_key(connection, 'abc123')
+
+    async def test_db_verify_api_key_unauthorized(self):
+        """Test the verification of api key: unauthorized key."""
+        connection = Connection(return_value=[])
+        with self.assertRaises(web.HTTPUnauthorized):
+            await db_verify_api_key(connection, 'abc123')
+
+    async def test_db_verify_admin_key_success(self):
+        """Test the verification of admin key: successful verification."""
+        connection = Connection(return_value=[{'comment': 'admin'}])
+        await db_verify_admin_key(connection, 'abc123')
+        # No exceptions raised
+
+    async def test_db_verify_admin_key_fail(self):
+        """Test the verification of admin key: failed to verify."""
+        connection = BadConnection()
+        with self.assertRaises(web.HTTPInternalServerError):
+            await db_verify_admin_key(connection, 'abc123')
+
+    async def test_db_verify_admin_key_unauthorized(self):
+        """Test the verification of admin key: unauthorized key."""
+        connection = Connection(return_value=[])
+        with self.assertRaises(web.HTTPUnauthorized):
+            await db_verify_admin_key(connection, 'abc123')
 
 
 if __name__ == '__main__':
