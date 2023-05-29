a7fe556d4df9c5c2087717d7cbf8ae528539a3c6
andrewpatto <andrew@patto.net>
2023-05-29 15:12:55 +1000

    Mustafas code of a new POST endpoint - applied to the latest code

--- aggregator/endpoints/query.py
+++ aggregator/endpoints/query.py
@@ -7,7 +7,7 @@ from aiohttp import web
 
 from ..config import CONFIG
 from ..utils.logging import LOG
-from ..utils.utils import get_access_token, get_services, query_service, parse_results
+from ..utils.utils import get_access_token, get_services, query_service, parse_results, post_query_service
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
@@ -30,6 +30,38 @@ async def send_beacon_query(request):
         else:
             task = asyncio.ensure_future(query_service(service, request.query_string, access_token))
             tasks.append(task)
+    # Prepare and initiate co-routines
+    results = await asyncio.gather(*tasks)
+
+    # Check if this aggregator is aggregating aggregators
+    # Aggregators return lists instead of objects, so they need to be broken down into a single list
+    if CONFIG.aggregators:
+        results = await parse_results(results)
+
+    return results
+
+
+async def post_beacon_query(request):
+    """Post Beacon queries and respond synchronously."""
+    LOG.debug("Normal response (sync), inside .............post_beacon_query")
+
+    jsonObj = request.headers["query"]
+    LOG.debug("json passed:......:"+str(jsonObj))
+
+    # Task variables
+    params = request.query_string  # query parameters (variant search)
+
+
+    tasks = []  # requests to be done
+    services = await get_services(request.host)  # service urls (beacons, aggregators) to be queried
+    access_token = await get_access_token(request)  # Get access token if one exists
+
+    for service in services:
+        # Generate task queue
+        LOG.debug(f"now calling service.........:{service}:params:{params}:access_token: {access_token}:request json:{request.headers['query']}")
+        task = asyncio.ensure_future(post_query_service(service, params, access_token, request.headers["query"]))
+        tasks.append(task)
+
     # Prepare and initiate co-routines
     results = await asyncio.gather(*tasks)
 
